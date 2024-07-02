6d188c71f63290aed8f3f097681b4ad955477593
ArleauxMakecode <120610871+ArleauxMakecode@users.noreply.github.com>
2024-07-02 17:39:24 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (cvzc == 1 && vccz == 1) {
+        // if8
+        music.play(music.stringPlayable("C5 B C5 B A G A B ", 120), music.PlaybackMode.UntilDone)
+        // if8
+        music.play(music.stringPlayable("A B C5 A C5 G A B ", 120), music.PlaybackMode.UntilDone)
+        soroban.showNumber(receivedNumber)
+    }
+})
 input.onGesture(Gesture.ScreenUp, function () {
     power.lowPowerEnable(LowPowerEnable.Prevent)
 })
@@ -12,85 +21,8 @@ input.onPinPressed(TouchPin.P1, function () {
     vccz = 1
 })
 let vccz = 0
-let cvzc = 1
-vccz = 0
-let list = images.createImage(`
-    . # # # #
-    # . . . .
-    . # # # .
-    . . . . #
-    # # # # .
-    `)
-let list1 = images.createImage(`
-    . . # . .
-    # . # . #
-    # . # . #
-    # . . . #
-    . # # # .
-    `)
-let list2 = images.createImage(`
-    . # # # .
-    # . . . .
-    # . # # #
-    # . . . .
-    . # # # .
-    `)
-let list3 = images.createImage(`
-    . . . . .
-    . . . . #
-    . # . # .
-    # . # . .
-    # # # # #
-    `)
-let list4 = images.createImage(`
-    # # . . .
-    # . # . .
-    # # . . .
-    # . # . .
-    # . . # .
-    `)
-let list5 = images.createImage(`
-    # # # # #
-    . . # . #
-    . . # . #
-    . . # . #
-    . . . # .
-    `)
-let list6 = images.createImage(`
-    # # # # .
-    # . . . #
-    # # # # .
-    # . . . .
-    # . . . .
-    `)
-let list7 = images.createBigImage(`
-    . # # # . . # # # #
-    # . . . # # . . . .
-    # . . . # . # # # .
-    # . . . # . . . . #
-    . # # # . # # # # .
-    `)
-let list8 = images.createImage(`
-    . . # . .
-    # . # . #
-    # . # . #
-    # . . . #
-    . # # # .
-    `)
-list.showImage(0)
-list1.showImage(0)
-list2.showImage(0)
-list3.showImage(0)
-list4.showImage(0)
-list5.showImage(0)
-list6.showImage(0)
-list7.scrollImage(1, 200)
-list8.showImage(0)
-images.iconImage(IconNames.Square).showImage(0)
-images.iconImage(IconNames.SmallSquare).showImage(0)
-images.iconImage(IconNames.Diamond).showImage(0)
-images.iconImage(IconNames.SmallDiamond).showImage(0)
-basic.clearScreen()
+let cvzc = 0
+radio.setGroup(1)
 basic.forever(function () {
     if (cvzc == 1 && vccz == 1) {
         led.plot(input.acceleration(Dimension.X), input.acceleration(Dimension.Y))
@@ -101,5 +33,12 @@ basic.forever(function () {
         // if8
         music.play(music.stringPlayable("A B C5 A C5 G A B ", 120), music.PlaybackMode.UntilDone)
         soroban.showNumber(randint(0, 20000))
+    }
+    if (cvzc == 1 && vccz == 1) {
+        // if8
+        music.play(music.stringPlayable("C5 B C5 B A G A B ", 120), music.PlaybackMode.UntilDone)
+        // if8
+        music.play(music.stringPlayable("A B C5 A C5 G A B ", 120), music.PlaybackMode.UntilDone)
+        radio.sendNumber(randint(0, 20000))
     }
 })
